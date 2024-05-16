8cf367069d5360bd6d089e734e05b745198228b2
jumaniyazovbobur <jumaniyazov.bobur@mail.ru>
2024-05-16 21:01:13 +0300

    CarService changed

--- src/CarService.java
+++ src/CarService.java
@@ -1,2 +1,7 @@
 public class CarService {
+   // git remote add origin https://github.com/jumaniyazovbobur/byte-git-lesson.git
+    // git push https://github.com/jumaniyazovbobur/byte-git-lesson.git
+    //   origin -> https://github.com/jumaniyazovbobur/byte-git-lesson.git
+    // git push origin
+    // git push -u origin master
 }
