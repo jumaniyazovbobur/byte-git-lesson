4e63c0c6ab9ad95dca7a7ceaa091f57bb6e6f6ad
jumaniyazovbobur <jumaniyazov.bobur@mail.ru>
2024-05-16 21:10:56 +0300

    Commit 4

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,11 @@
 public class Car {
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
